c04bc4dae9a6d47ae6936b786069d1608192066a
Drelf2018 <drelf2018@outlook.com>
2024-06-27 02:42:01 +0800

    feat: 确保实现了 io.ReaderFrom

--- utils/message.go
+++ utils/message.go
@@ -7,11 +7,6 @@ import (
 
 type Message []byte
 
-func (m Message) ReadFrom(r io.Reader) (int64, error) {
-	n, err := r.Read(m)
-	return int64(n), err
-}
-
 func (m Message) Ver() uint16 {
 	return binary.BigEndian.Uint16(m[WsVerBegin:WsVerEnd])
 }
@@ -20,6 +15,13 @@ func (m Message) Op() uint32 {
 	return binary.BigEndian.Uint32(m[WsOpBegin:WsOpEnd])
 }
 
+func (m Message) ReadFrom(r io.Reader) (int64, error) {
+	n, err := r.Read(m)
+	return int64(n), err
+}
+
 func (m Message) Write(p []byte) (n int, err error) {
 	return copy(p, m), nil
 }
+
+var _ io.ReaderFrom = &Message{}
